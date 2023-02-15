6ee1deb6e98287a23b09d452b8858cd70db5be1a
Dumik121 <nazar121duma@gmail.com>
2023-02-15 12:11:06 +0200

    feat: concatMatrix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let arr=[];
+  for(let k=1;k<matrix.length;k+=2) {matrix[k].reverse();}
+  return arr = [].concat.apply([], matrix);
 }
